c0328173a887152fbf49e3ebc6c5176a46f8197b
walidbouguima <walid.bouguima@gmail.com>
2020-10-23 16:18:59 +0100

    Add the logic to display the cart total on the shopping_cart badge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
+import { CartService } from './cart.service';
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import {ProductsService} from './products.service'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,8 +11,13 @@ import {ProductsService} from './products.service'
 export class AppComponent {
 
   products$: Observable<any>;
-
-  constructor(private ps : ProductsService){
+  cart$ : Observable<any>
+  cart;
+  constructor(private ps : ProductsService, private cartService: CartService){
     this.products$ = this.ps.getProducts()
+    this.cart$ = this.cartService.cart$.subscribe(cart => this.cart = cart);
+  }
+  onAddProduct(count, product){
+    this.cartService.addToCart(count, product)
   }
 }
