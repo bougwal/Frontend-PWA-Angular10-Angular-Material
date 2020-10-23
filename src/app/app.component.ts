9006bdf8368d058a681d48581c3317b924250fb3
walidbouguima <walid.bouguima@gmail.com>
2020-10-23 15:50:22 +0100

    Scaffold a service and a new component. I hooked up the data with async styled the card and configured he layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import {ProductsService} from './products.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'es';
+
+  products$: Observable<any>;
+
+  constructor(private ps : ProductsService){
+    this.products$ = this.ps.getProducts()
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import {MatBadgeModule}from '@angular/material/badge';
+import {MatButtonModule}from '@angular/material/button';
+import {MatCardModule}from '@angular/material/card';
+import {MatDialogModule}from '@angular/material/dialog';
+import {MatIconModule}from '@angular/material/icon';
+import {MatListModule}from '@angular/material/list';
+import {MatSelectModule}from '@angular/material/select';
+import {MatSidenavModule}from '@angular/material/sidenav';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import  {MatToolbarModule} from '@angular/material/toolbar';
 import { NavComponent } from './nav/nav.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
+import { NgModule } from '@angular/core';
+import { AddToCartComponent } from './add-to-cart/add-to-cart.component';
 
+const matDesignModules = [ MatToolbarModule, MatButtonModule,
+    MatSidenavModule, MatIconModule, MatListModule, MatCardModule,
+    MatSelectModule, MatDialogModule, MatBadgeModule, MatSnackBarModule ];
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    AddToCartComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule
+    HttpClientModule,
+    ...matDesignModules
   ],
   providers: [],
   bootstrap: [AppComponent]
