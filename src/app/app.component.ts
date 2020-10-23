d2f29e6c1ee7c23b0e69ec2f65b81c0b8927bc75
walidbouguima <walid.bouguima@gmail.com>
2020-10-23 16:50:16 +0100

    Add cart MatDialog component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
+import { CartComponent } from './cart/cart.component';
 import { CartService } from './cart.service';
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Observable } from 'rxjs';
 import {ProductsService} from './products.service'
 
@@ -13,11 +15,18 @@ export class AppComponent {
   products$: Observable<any>;
   cart$ : Observable<any>
   cart;
-  constructor(private ps : ProductsService, private cartService: CartService){
+  constructor(private ps : ProductsService, private cartService: CartService , private dialgo: MatDialog) {
     this.products$ = this.ps.getProducts()
     this.cart$ = this.cartService.cart$.subscribe(cart => this.cart = cart);
   }
   onAddProduct(count, product){
     this.cartService.addToCart(count, product)
   }
+
+  openCart(){
+    this.dialgo.open(CartComponent, {
+      width: '500px'
+    })
+  }
 }
+
