c68ec5db0c1e0f8b4dbe80da11c5a914d9fd65d6
walidbouguima <walid.bouguima@gmail.com>
2020-10-28 10:27:58 +0100

    Add momentum-ui to es-admin application

--- apps/es-admin/src/app/app.module.ts
+++ apps/es-admin/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { BadgeModule } from '@momentum-ui/angular';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    BadgeModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
