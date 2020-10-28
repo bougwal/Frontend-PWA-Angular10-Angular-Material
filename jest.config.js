9ec90db666d0506af6b909f0d7ddc2f6ddb4d3ad
walidbouguima <walid.bouguima@gmail.com>
2020-10-28 10:01:07 +0100

    Create a library named shared using Nx

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/apps/es-api', '<rootDir>/apps/es-admin'],
+  projects: [
+    '<rootDir>/apps/es-api',
+    '<rootDir>/apps/es-admin',
+    '<rootDir>/libs/shared',
+  ],
 };
